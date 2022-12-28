0620b6a361d0234fd5e9afb0dae2e7571ecc8720
yangshaohe <53420482+yangshaohe@users.noreply.github.com>
2022-12-28 16:10:33 +0800

    C_test
    C语言学习

---
+++ Test_12_28/Test_12_28/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+//³£Á¿
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[] = {'a','b','c'};
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", strlen(arr2));
+	return 0;
+
+}
